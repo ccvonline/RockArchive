de050a5cfd3d3fb148f6ed2a86449517f23d1158
David Turner <david@sparkdevnetwork.org>
2015-04-20 04:42:48 -0700

    Latest from Core
    Former-commit-id: 4bc5585df773f55520e9ec2ab784e75edff5b808

--- Rock/Attribute/GroupAndRoleFieldAttribute.cs
+++ Rock/Attribute/GroupAndRoleFieldAttribute.cs
@@ -22,32 +22,27 @@ using System.Web;
 namespace Rock.Attribute
 {
     /// <summary>
-    /// Field Attribute to select a workflow attribute from a workflow type.
+    /// Field Attribute to select group and role filtered by a group type
+    /// Stored as "GroupType.Guid|Group.Guid"
     /// </summary>
     [AttributeUsage( AttributeTargets.Class, AllowMultiple = true, Inherited = true )]
-    public class WorkflowAttributeAttribute : FieldAttribute
+    public class GroupAndRoleFieldAttribute : FieldAttribute
     {
-        private const string ATTRIBUTE_FIELD_TYPES_KEY = "attributefieldtypes";
-
         /// <summary>
-        /// Initializes a new instance of the <see cref="WorkflowAttributeAttribute" /> class.
+        /// Initializes a new instance of the <see cref="DefinedValueFieldAttribute" /> class.
         /// </summary>
         /// <param name="name">The name.</param>
         /// <param name="description">The description.</param>
+        /// <param name="groupAndRolePickerLabel">The group picker label.</param>
         /// <param name="required">if set to <c>true</c> [required].</param>
         /// <param name="defaultValue">The default value.</param>
         /// <param name="category">The category.</param>
         /// <param name="order">The order.</param>
         /// <param name="key">The key.</param>
-        /// <param name="fieldTypeClassNames">The field type class names which are used to filter the pickable workflow attributes</param>
-        public WorkflowAttributeAttribute( string name, string description = "", bool required = true, string defaultValue = "", string category = "", int order = 0, string key = null, string[] fieldTypeClassNames = null )
-            : base( name, description, required, defaultValue, category, order, key, typeof( Rock.Field.Types.WorkflowAttributeFieldType ).FullName )
+        public GroupAndRoleFieldAttribute( string name = "", string description = "", string groupAndRolePickerLabel = "", bool required = true, string defaultValue = "", string category = "", int order = 0, string key = null )
+            : base( name, description, required, defaultValue, category, order, key, typeof( Rock.Field.Types.GroupAndRoleFieldType ).FullName )
         {
-            if ( fieldTypeClassNames != null && fieldTypeClassNames.Length > 0 )
-            {
-                var workflowTypeConfigValue = new Field.ConfigurationValue( fieldTypeClassNames.ToList().AsDelimited( "|" ) );
-                FieldConfigurationValues.Add( ATTRIBUTE_FIELD_TYPES_KEY, workflowTypeConfigValue );
-            }
+            FieldConfigurationValues.Add( Rock.Field.Types.GroupAndRoleFieldType.CONFIG_GROUP_AND_ROLE_PICKER_LABEL, new Field.ConfigurationValue( groupAndRolePickerLabel ) );
         }
     }
 }
