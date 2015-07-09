ccvonline/RockArchive

commit 753356aad92f8db1e24b29e19b88b64305047689

- Fixed models overriding parent authority to do so in a way that they still get the GlobalDefault security settings
Former-commit-id: 8ca7e7acc1cf4bec6468965fbafad67e8d8d9c94

Rock/Model/Block.cs
@@ -242,11 +242,11 @@ namespace Rock.Model
             {
                 if ( this.BlockLocation == Model.BlockLocation.Page )
                 {
-                    return this.Page;
+                    return this.Page != null ? this.Page : base.ParentAuthority;
                 }
                 else
                 {
-                    return this.Layout;
+                    return this.Layout != null ? this.Page : base.ParentAuthority;
                 }
             }
         }

Rock/Model/BinaryFile.cs
@@ -432,7 +432,7 @@ namespace Rock.Model
         {
             get
             {
-                return this.BinaryFileType;
+                return this.BinaryFileType != null ? this.BinaryFileType : base.ParentAuthority;
             }
         }
 

Rock/Model/ContentChannel.cs
@@ -189,7 +189,7 @@ namespace Rock.Model
         {
             get
             {
-                return this.ContentChannelType;
+                return this.ContentChannelType != null ? this.ContentChannelType : base.ParentAuthority;
             }
         }
 

Rock/Model/ContentChannelItem.cs
@@ -178,7 +178,7 @@ namespace Rock.Model
         {
             get
             {
-                return ContentChannel;
+                return ContentChannel != null ? ContentChannel : base.ParentAuthority;
             }
         }
 

Rock/Model/DefinedValue.cs
@@ -108,7 +108,10 @@ namespace Rock.Model
         /// </value>
         public override Security.ISecured ParentAuthority
         {
-            get { return this.DefinedType; }
+            get 
+            {
+                return this.DefinedType != null ? this.DefinedType : base.ParentAuthority;
+            }
         }
 
         /// <summary>

Rock/Model/EntitySetItem.cs
@@ -117,7 +117,10 @@ namespace Rock.Model
         /// </summary>
         public override Security.ISecured ParentAuthority
         {
-            get { return this.EntitySet; }
+            get 
+            {
+                return this.EntitySet != null ? this.EntitySet : base.ParentAuthority;
+            }
         }
 
         #endregion

Rock/Model/EventCalendarItem.cs
@@ -88,7 +88,7 @@ namespace Rock.Model
         {
             get
             {
-                return this.EventCalendar;
+                return this.EventCalendar != null ? this.EventCalendar : base.ParentAuthority;
             }
         }
         #endregion

Rock/Model/Group.cs
@@ -387,7 +387,7 @@ namespace Rock.Model
         {
             get
             {
-                return this.ParentGroup;
+                return this.ParentGroup != null ? this.ParentGroup : base.ParentAuthority;
             }
         }
 

commit aade9f0ff6cd56952d9f1a97c6c56d2b59fbe405

- fix block parent auth incorrect when part of a Layout (introduced in https://github.com/SparkDevNetwork/Rock/commit/753356aad92f8db1e24b29e19b88b64305047689 [formerly 8ca7e7acc1cf4bec6468965fbafad67e8d8d9c94] a couple of days ago :))
Former-commit-id: 7cd997b4e550aac04838eabfd0ee1442132a4d1a

Rock/Model/Block.cs
@@ -246,7 +246,7 @@ namespace Rock.Model
                 }
                 else
                 {
-                    return this.Layout != null ? this.Page : base.ParentAuthority;
+                    return this.Layout != null ? this.Layout : base.ParentAuthority;
                 }
             }
         }