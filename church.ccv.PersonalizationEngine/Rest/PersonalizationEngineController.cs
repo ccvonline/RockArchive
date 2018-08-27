536fbec7f5be3414ff0e16ccd8d1c1c10ed5465d
jeredmcferron@gmail.com <jeredmcferron@gmail.com>
2018-08-27 13:19:23 -0700

    Updated PersonalizationEngine REST
    Added utility function for converting string enums to an enum array
    Added GetDefaultCampaign for use when the person is unknown
    
    
    Former-commit-id: 8dea8faa0fc2af6f558df0085d8c34d808bb427c

--- church.ccv.PersonalizationEngine/Rest/PersonalizationEngineController.cs
+++ church.ccv.PersonalizationEngine/Rest/PersonalizationEngineController.cs
@@ -15,20 +15,15 @@ namespace church.ccv.PersonalizationEngine.Rest
         [Authenticate, Secured]
         public HttpResponseMessage GetRelevantCampaign( string campaignTypeList, int personId )
         {
-            // convert the string list into an array of enums
-            string[] types = campaignTypeList.Split( ',' );
-
-            CampaignType[] campaignTypeEnumList = new CampaignType[types.Length];
-            for( int i = 0; i < types.Length; i++ )
-            {
-                campaignTypeEnumList[ i ] = (CampaignType)Enum.Parse( typeof( CampaignType ), types[ i ], true );
-            }
+            // convert the strings to enum
+            CampaignType[] campaignTypeEnumList = StringToEnum( campaignTypeList );
 
             // now request the relevant campaign for the person
             var campaignResult = PersonalizationEngineUtil.GetRelevantCampaign( campaignTypeEnumList, personId );
+
+            // and if nothing was found, take a default
             if( campaignResult == null )
             {
-                // and if nothign was found, take a default.
                 var defaultCampaigns = PersonalizationEngineUtil.GetDefaultCampaigns( campaignTypeEnumList );
                 campaignResult = defaultCampaigns[ 0 ];
             }
@@ -38,6 +33,39 @@ namespace church.ccv.PersonalizationEngine.Rest
             {
                 Content = restContent
             };
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route( "api/PersonalizationEngine/DefaultCampaign" )]
+        [Authenticate, Secured]
+        public HttpResponseMessage GetDefaultCampaign( string campaignTypeList )
+        {
+            // convert the strings to enum
+            CampaignType[] campaignTypeEnumList = StringToEnum( campaignTypeList );
+
+            // now grab a default campaign
+            var defaultCampaigns = PersonalizationEngineUtil.GetDefaultCampaigns( campaignTypeEnumList );
+            var campaignResult = defaultCampaigns[ 0 ];
+
+            StringContent restContent = new StringContent( JsonConvert.SerializeObject( campaignResult ), Encoding.UTF8, "application/json" );
+            return new HttpResponseMessage()
+            {
+                Content = restContent
+            };
+        }
+
+        CampaignType[] StringToEnum( string campaignTypeList )
+        {
+            // convert the string list into an array of enums
+            string[] types = campaignTypeList.Split( ',' );
+
+            CampaignType[] campaignTypeEnumList = new CampaignType[types.Length];
+            for( int i = 0; i < types.Length; i++ )
+            {
+                campaignTypeEnumList[ i ] = (CampaignType)Enum.Parse( typeof( CampaignType ), types[ i ], true );
+            }
+
+            return campaignTypeEnumList;
         }
     }
 }
