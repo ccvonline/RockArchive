183c5a2faf7ed5277185b460fa1bcf5648dc4cc1
jeredmcferron@gmail.com <jeredmcferron@gmail.com>
2018-10-15 11:12:08 -0700

    Personalization Engine commits
    Former-commit-id: a6ee9d7ff041625452516bb8fdf049b369eafe5e

---
+++ RockWeb/Plugins/church_ccv/PersonalizationEngine/PersonaDetail.ascx
@@ -0,0 +1,161 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PersonaDetail.ascx.cs" Inherits="RockWeb.Plugins.church_ccv.PersonalizationEngine.PersonaDetail" %>
+
+<asp:UpdatePanel ID="upnlSettings" runat="server">
+    <ContentTemplate>
+        <div ID="error-panel" class="alert alert-danger" style="visibility: hidden; position: fixed; z-index:9999; left: 50%; top: 50%; transform: translate( -50%, -50%);">
+            <p><i class="fa fa-times"></i> Fill in the fields colored red and try again.</p>
+        </div>
+
+        <div class ="panel panel-block">
+            <div class="panel-heading">
+                <div class="row col-sm-4">
+                    <h4 class="panel-title">Persona</h4>
+                </div>
+            </div>
+
+            <div class="panel-body">
+                <div class="persona-editable-item">
+                    <asp:Literal runat="server"><p class="title">Persona Name</p></asp:Literal>
+                    <Rock:RockTextBox runat="server" ClientIDMode="Static" ID="tbPersonaName"></Rock:RockTextBox>
+                </div>
+                <br/>
+                <div class="persona-editable-item">
+                    <asp:Literal runat="server"><p class="title">Persona Description</p></asp:Literal>
+                    <Rock:RockTextBox runat="server" ClientIDMode="Static" ID="tbPersonaDesc"></Rock:RockTextBox>
+                </div>
+                <br/>
+                <div class="persona-editable-item">
+                    <asp:Literal runat="server"><p class="title">Persona Rock SQL</p></asp:Literal>
+                    <Rock:RockTextBox runat="server" ClientIDMode="Static" TextMode="MultiLine" Rows="10" ID="tbPersonaRockSQL"></Rock:RockTextBox>
+                </div>
+                <br/>
+                <asp:LinkButton ID="btnSave" ClientIDMode="Static" runat="server" Text="Save" CssClass="btn btn-primary" OnClientClick="return validateFields();" OnClick="btnSave_Click" />
+            </div>
+        </div>
+    </ContentTemplate>
+</asp:UpdatePanel>
+
+<asp:UpdatePanel ID="upnlCampaigns" runat="server">
+    <ContentTemplate>
+        <div class ="panel panel-block">
+            <div class="panel-heading">
+                <div class="row col-sm-4">
+                    <h4 class="panel-title">Campaigns</h4>
+                </div>
+            </div>
+
+            <div class="panel-body">
+                <Rock:Grid ID="gCampaigns" Title="Attached Campaigns" runat="server" DisplayType="Light" AllowSorting="true" RowItemText="Campaign" AllowPaging="false" OnRowSelected="CampaignsGrid_RowSelected">
+                    <Columns>
+                        <Rock:RockBoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                        <Rock:RockBoundField DataField="Description" HeaderText="Description" SortExpression="Description" />
+                        <Rock:DeleteField OnClick="CampaignsGrid_Delete" />
+                    </Columns>
+                </Rock:Grid>
+            </div>
+        </div>
+
+        <Rock:ModalAlert ID="maWarning" runat="server" />
+        <Rock:ModalDialog ID="mdAddCampaign" runat="server" ScrollbarEnabled="true" SaveButtonText="Add" OnSaveClick="mdAddCampaign_AddClick"  Title="Select Campaign">
+            <Content>
+                <Rock:RockDropDownList ID="ddlCampaign" runat="server" Label="Select Campaign" />
+            </Content>
+        </Rock:ModalDialog>
+    </ContentTemplate>
+</asp:UpdatePanel>
+
+<style>
+    .persona-editable-item{
+        display: flex;
+        justify-content: flex-start;
+    }
+    
+    .persona-editable-item .title {
+        min-width: 150px;
+    }
+
+    .persona-types {
+    }
+
+    .persona-type {
+        display: flex;
+        flex-direction: column;
+        margin-top: 25px;
+
+        border-width: 1px;
+        border-color: #b8b2aa;
+        border-style: solid;
+        border-radius: 4px;
+        padding: 5px;
+    }
+
+    .persona-type-header {
+    }
+
+    .persona-type-template-item {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .form-control {
+        width: 500px;
+    }
+
+    .error {
+        border-width: 3px;
+        border-color: #9d3f3d;
+        border-style: solid;
+    }
+</style>
+
+<script>
+    function pageLoad() {
+
+        // setup event handlers for input that clear the red error box around fields.
+        $("#tbPersonaName").on('input', function () {
+            $(this).removeClass("error");
+        });
+
+        $("#tbPersonaDesc").on('input', function () {
+            $(this).removeClass("error");
+        });
+    }
+
+    function validateFields() {
+        var success = true;
+
+        // validate the persona name
+        if (validateField("#tbPersonaName") == false) {
+            success = false;
+        }
+
+        // validate the persona description
+        if (validateField("#tbPersonaDesc") == false) {
+            success = false;
+        }
+
+        if (success == false) {
+            $("#error-panel").css("visibility", "visible");
+            setTimeout(hideAlert, 5000);
+        }
+
+        return success;
+    }
+
+    function validateField(fieldId) {
+        var fieldVal = $(fieldId).val();
+
+        if (fieldVal) {
+            $(fieldId).removeClass("error");
+            return true;
+        }
+        else {
+            $(fieldId).addClass("error");
+            return false;
+        }
+    }
+
+    function hideAlert() {
+        $("#error-panel").css("visibility", "hidden");
+    }
+</script>
---
+++ RockWeb/Plugins/church_ccv/PersonalizationEngine/PersonaList.ascx.cs
@@ -0,0 +1,179 @@
+﻿
+using System;
+using System.ComponentModel;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Data;
+using church.ccv.PersonalizationEngine.Model;
+using Rock.Web.UI.Controls;
+using Rock;
+using Rock.Security;
+using System.Linq;
+using System.Data.Entity;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+using Rock.Attribute;
+using church.ccv.PersonalizationEngine.Data;
+
+namespace RockWeb.Plugins.church_ccv.PersonalizationEngine
+{
+    [DisplayName( "Persona List" )]
+    [Category( "CCV > Personalization Engine" )]
+    [Description( "Displays existing personas in a list." )]
+
+    [LinkedPage("Detail Page")]
+    public partial class PersonaList : RockBlock
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            PersonaFilter_Init( );
+            PersonaGrid_Init( );
+        }
+        
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                PersonaFilter_Bind( );
+                PersonaGrid_Bind( );
+            }
+        }
+
+        #region Persona Filter
+        protected void PersonaFilter_Init( )
+        {
+            rPersonaFilter.ApplyFilterClick += PersonaFilter_ApplyFilterClick;
+        }   
+
+        protected void PersonaFilter_ApplyFilterClick( object sender, EventArgs e )
+        {
+            rPersonaFilter.SaveUserPreference( "Title", filterTbTitle.Text );
+
+            PersonaGrid_Bind( );
+        }
+
+        protected void PersonaFilter_DisplayFilterValue( object sender, GridFilter.DisplayFilterValueArgs e )
+        {
+            switch( e.Key )
+            {
+                case "Title":
+                {
+                    e.Value = filterTbTitle.Text;
+                    break;
+                }
+
+                default:
+                {
+                    e.Value = string.Empty;
+                    break;
+                }
+            }
+        }
+        
+        protected void PersonaFilter_Bind( )
+        {
+            filterTbTitle.Text = rPersonaFilter.GetUserPreference( "Title" );
+        }
+        #endregion
+
+        #region Persona Grid
+        protected void PersonaGrid_Init( )
+        {
+            gPersonaGrid.DataKeyNames = new string[] { "Id" };
+
+            // turn on only the 'add' button
+            gPersonaGrid.Actions.Visible = true;
+            gPersonaGrid.Actions.Enabled = true;
+            gPersonaGrid.Actions.ShowBulkUpdate = false;
+            gPersonaGrid.Actions.ShowCommunicate = false;
+            gPersonaGrid.Actions.ShowExcelExport = false;
+            gPersonaGrid.Actions.ShowMergePerson = false;
+            gPersonaGrid.Actions.ShowMergeTemplate = false;
+            
+            gPersonaGrid.Actions.ShowAdd = IsUserAuthorized( Authorization.EDIT );
+            gPersonaGrid.Actions.AddClick += PersonaGrid_AddClick;
+
+            gPersonaGrid.GridRebind += PersonaGrid_GridRebind;
+        }
+
+        protected void PersonaGrid_AddClick( object sender, EventArgs e )
+        {
+            NavigateToDetailPage( null );
+        }
+
+        protected void PersonaGrid_RowSelected( object sender, RowEventArgs e )
+        {
+            NavigateToDetailPage( e.RowKeyId );
+        }
+
+        protected void PersonaGrid_Remove( object sender, RowEventArgs e )
+        {
+            PersonalizationEngineUtil.DeletePersona( e.RowKeyId );
+
+            PersonaGrid_Bind( );
+        }
+
+        protected void PersonaGrid_GridRebind( object sender, GridRebindEventArgs e )
+        {
+            PersonaGrid_Bind( );
+        }
+
+        protected void PersonaGrid_Bind( )
+        {
+            // Grab all the personas
+            using ( RockContext rockContext = new RockContext( ) )
+            {
+                var personaQuery = new Service<Persona>( rockContext ).Queryable( );
+
+                // ---- Apply Filters ----
+
+                // --Title
+                if( string.IsNullOrWhiteSpace( filterTbTitle.Text ) == false )
+                {
+                    personaQuery = personaQuery.Where( c => c.Name.ToLower( ).Contains( filterTbTitle.Text.ToLower( ) ) );
+                }
+                
+                // ---- Load Data ----
+                var dataSource = personaQuery.Select( c => new
+                    {
+                        Id = c.Id,
+                        Name = c.Name,
+                        Description = c.Description
+                    });
+
+                // --- Sorting ---
+                if( gPersonaGrid.SortProperty != null )
+                {
+                    dataSource = dataSource.Sort( gPersonaGrid.SortProperty );
+                }
+
+                gPersonaGrid.DataSource = dataSource.ToList( );
+                gPersonaGrid.DataBind( );
+            }
+        }
+        #endregion
+
+        #region Utility
+        protected void NavigateToDetailPage( int? personaId )
+        {
+            var qryParams = new Dictionary<string, string>();
+            int personaQueryId = personaId.HasValue ? personaId.Value : 0;
+            qryParams.Add( "PersonaId", personaQueryId.ToString( ) );
+            
+            NavigateToLinkedPage( "DetailPage", qryParams );
+        }
+        #endregion
+    }
+}
