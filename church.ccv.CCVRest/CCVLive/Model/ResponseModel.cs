0eec5867ea5b071757e0cc945daa96aa5f124cba
BEN TWEEDY <ben@btpaz.com>
2018-12-20 16:02:16 -0700

    add ccv rest with ccv live api controller
    Former-commit-id: 90dc542fedc90baf874877393bb988e445b183f5

---
+++ church.ccv.CCVRest/CCVLive/Model/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace church.ccv.CCVRest.CCVLive.Model
+{
+    [Serializable]
+    class ResponseModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string Data { get; set; }
+    }
+}
