09aecd9a0ad37535fa4e11b8c4689b28edffac75
Adolfo Segura <adolfo.segura20@outlook.com>
2019-02-19 11:44:54 -0700

    Latest Updates
    Former-commit-id: 94090fc4643ca48eac4ba74436c688e641998f32

--- RockWeb/Plugins/church_ccv/SafetySecurity/CCVMinistrySafe.ascx.cs
+++ RockWeb/Plugins/church_ccv/SafetySecurity/CCVMinistrySafe.ascx.cs
@@ -29,19 +29,19 @@ namespace RockWeb.Plugins.church_ccv.SafetySecurity
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public string Email { get; set; }
-            public string MinistrySafeStatus { get; set; }
-            public string CriminalBackgroundChecklevel { get; set; }
-            public string CriminalBackgroundCheckStatus { get; set; }
-            public string Tag { get; set; }
-            public string ClassificationEmpVol { get; set; }
-            public string Charges { get; set; }
-            public string Application { get; set; }
-            public string References { get; set; }
-            public string Interview { get; set; }
-            public string RenewaldateSexualAbuseAwarenessTraining { get; set; }
-            public string Role { get; set; }
-            public DateTime RenewalDateCriminalBackgroundCheck { get; set; }
+            public string SexualAbuseAwarenessTraining { get; set; }
             public int TrainingScore { get; set; }
+            public DateTime RenewalDateSexualAbuseAwarenessTraining { get; set; }
+            //public string CriminalBackgroundChecklevel { get; set; }
+            //public string CriminalBackgroundCheckStatus { get; set; }
+            //public string Tag { get; set; }
+            //public string ClassificationEmpVol { get; set; }
+            //public string Charges { get; set; }
+            //public string Application { get; set; }
+            //public string References { get; set; }
+            //public string Interview { get; set; }
+            //public string Role { get; set; }
+            //public DateTime RenewalDateCriminalBackgroundCheck { get; set; }
 
         }
 
@@ -53,102 +53,84 @@ namespace RockWeb.Plugins.church_ccv.SafetySecurity
         public List<MinistrySafePerson> LoadCsvFile<MinistrySafePerson>( string filePath )
         {
             var rockContext = new RockContext();
-
             PersonService personService = new PersonService( rockContext );
-            
 
             using ( var reader = new StreamReader( Directory.GetCurrentDirectory() + @"\file.csv" ) ) //Directory.GetCurrentDirectory() + @"\file.csv";
             using ( var csvReader = new CsvReader( reader ) )
             {
                 csvReader.Configuration.HasHeaderRecord = true;
                 var peopleList = csvReader.GetRecords<MinistrySafePerson>().ToList();
-                Person person = null;
+                Person currentPerson = null;
 
-
-                foreach ( var row in peopleList )
+                foreach ( var person in peopleList )
                 {
-                    MinistrySafePerson roww;
-                    
-                    string firstName = roww.FirstName;
-                    string lastName = tbLastName.Text.Trim();
-                    string email = tbEmail.Text.Trim();
+                    // MinistrySafePerson roww;
+
+                    string firstName = ""; //person.FirstName;
+                    string lastName = ""; //person.LastName;
+                    string email = ""; //person.Email;
+                    int trainingScore = 5; //person.TrainingScore;
+                    string sexualAbuseAwarenessTraining = ""; // person.SexualAbuseAwarenessTraining;
+                    DateTime renewalDateSexualAbuseAwarenessTraining = DateTime.Now; //person.RenewalDateSexualAbuseAwarenessTraining;
 
                     // Try to find matching person
-                    var personMatches = personService.GetByMatch( row.FirstName, row.LastName, row.Email );
+                    var personMatches = personService.GetByMatch( firstName, lastName, email );
                     if ( personMatches.Count() == 1 )
                     {
                         // If one person with same name and email address exists, use that person
-                        person = personMatches.First();
+                        currentPerson = personMatches.First();
                     }
-              
+
                     // If person was not found, create a new one
                     else if ( person == null )
-                    {   
-                        person = new Person();
-                        person.FirstName = firstName;
-                        person.LastName = lastName;
-                        person.Email = email;
-                        PersonService.SaveNewPerson( person, rockContext);
+                    {
+                        currentPerson = new Person();
+                        currentPerson.FirstName = firstName;
+                        currentPerson.LastName = lastName;
+                        currentPerson.Email = email;
+                        PersonService.SaveNewPerson( currentPerson, rockContext );
                     }
 
-
-
-
                     // If there is a valid person with a primary alias, continue
-                    if ( person != null && person.PrimaryAliasId.HasValue )
+                    if ( currentPerson != null && currentPerson.PrimaryAliasId.HasValue )
                     {
+                        // Get attributes in rock
+                        var ministrySafeResult = currentPerson.AttributeValues[ "MinistrySafeResult" ].ToString(); // Pass or Fail
+                        var ministrySafeStatus = currentPerson.AttributeValues[ "MinistrySafeStatus"].ToString(); // Complete or Incomplete
+                        DateTime? ministrySafeRenewalDate = currentPerson.GetAttributeValue( "MinistrySafeRenewalDate" ).AsDateTime();
 
-                        // Get next family members details
-                        var ministrySafeResult = person.GetAttributeValues( "MinistrySafeResult" );
-                        DateTime? ministrySafeRenewalDate = person.GetAttributeValue( "MinistrySafeRenewalDate" ).AsDateTime();
-                        string ministrySafeStatus = person.AttributeValues["MinistrySafeStatus"].ToString();
-
-                        // Assign baptism photo
-                        if ( SexualAbuseAwarenessTrainingCompletedIncompleted == 'Complete' && TrainingScore >= 70 )
+                        // If the training has been completed.
+                        if ( sexualAbuseAwarenessTraining == "Complete" )
                         {
-                            ministrySafeResult.Text = "Pass";
+                            // If the score is greater than or equal to 70.
+                            if ( trainingScore >= 70 )
+                            {
+                                ministrySafeResult = "Pass";
+                                ministrySafeStatus = "Complete";
+                                ministrySafeRenewalDate = renewalDateSexualAbuseAwarenessTraining;
+                            }
+                            // If the score is less than 70
+                            else if ( trainingScore < 70 )
+                            {
+                                ministrySafeResult = "Fail";
+                                ministrySafeStatus = "Complete";
+                                ministrySafeRenewalDate = null;
+                            }
                         }
                         else
                         {
-                            lBaptismPhoto.Text = "";
+                            // If training is not complete, mark status Incomplete, and both result and renewal date fields will be blank
+                            ministrySafeStatus = "Incomplete";
+                            ministrySafeResult = null;
+                            ministrySafeRenewalDate = null;
                         }
+                        return peopleList;
+                    }
+                }
 
-
-                        return people;
+                return peopleList;
             }
-
         }
-      
 
-
-
-
-
-
-
-
-
-
-    //private List<MinistrySafePerson> LoadCsvFile<MinistrySafePerson>( string filePath )
-    //{
-
-    //    var fileName = Path.Combine( this.SlingshotDirectoryName, new T().GetFileName() );
-    //    if ( File.Exists( fileName ) )
-    //    {
-    //        using ( var fileStream = File.OpenText( fileName ) )
-    //        {
-    //            CsvReader csvReader = new CsvReader( fileStream );
-    //            csvReader.Configuration.HasHeaderRecord = true;
-    //            return csvReader.GetRecords<MinistrySafePerson>().ToList();
-    //        }
-    //    }
-    //    else
-    //    {
-    //        return new List<MinistrySafePerson>();
-    //    }
-    //}
-
-
-
-}
+    }
 }
