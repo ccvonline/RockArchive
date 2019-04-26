e1336e6f64ce0d72f758cc5a6e2af7f8594e0ad5
Jered McFerron <jeredmcferron@gmail.com>
2019-04-26 09:41:44 -0700

    Suppress "value never assigned" warnings for this model, since it's filled via reflection
    Former-commit-id: f5337c1db3507bb316b44da66bfe6c7fd0686d2e

--- church.ccv.CCVRest/MobileApp/Model/CCVLiveCountdownModel.cs
+++ church.ccv.CCVRest/MobileApp/Model/CCVLiveCountdownModel.cs
@@ -5,8 +5,10 @@ namespace church.ccv.CCVRest.MobileApp.Model
 {
     class CCVLiveCountdownModel
     {
+#pragma warning disable 0649 //suppress the "value never assigned" warnings, since we fill this via json deserializing
         public bool IsLive;
    
         public DateTime? EventStartTime;
+#pragma warning restore 0649
     }
 }
