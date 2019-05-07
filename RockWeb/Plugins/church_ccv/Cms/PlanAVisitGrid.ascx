cc490a2df115a2f3e8afddae278444246756ce5c
cupshir <christopherupshir@ccv.church>
2019-05-06 17:27:28 -0700

    Plan A Visit Grid block initial commit.  Work In Progress.
    Former-commit-id: b61243575bdaea853de268bf664ed45358546c25

---
+++ RockWeb/Plugins/church_ccv/Cms/PlanAVisitGrid.ascx
@@ -0,0 +1,52 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PlanAVisitGrid.ascx.cs" Inherits="RockWeb.Plugins.church_ccv.Cms.PlanAVisitGrid" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pVolunteerScreeningList" runat="server" class="panel panel-block">
+            <div class="panel-heading">
+                <div class="row col-sm-4">
+                    <h4 class="panel-title">Plan A Visit</h4>
+                    <br />
+                </div>
+            </div>
+            <div class="panel-body">
+                <div class="grid grid-panel">
+
+                    <Rock:Grid ID="gGrid" runat="server" AutoGenerateColumns="False" OnRowSelected="gGrid_RowSelected">
+                        <Columns>
+                            <Rock:RockBoundField DataField="ScheduledDate" HeaderText="Scheduled Date" DataFormatString="{0:MM/d/yyyy}" />
+                            <Rock:RockBoundField DataField="ServiceTime" HeaderText="Service Time" />
+                            <Rock:RockBoundField DataField="Campus" HeaderText="Campus"/>
+                            <Rock:RockBoundField DataField="Person" HeaderText="Person" HtmlEncode="false" />
+                            <Rock:RockBoundField DataField="BringingSpouse" HeaderText="Bringing Spouse" />                    
+                            <Rock:RockBoundField DataField="BringingChildren" HeaderText="Bringing Kids" />
+                            <Rock:RockBoundField DataField="AttendedDate" HeaderText="Attended Date" DataFormatString="{0:MM/d/yyyy}" />
+                            <Rock:DeleteField OnClick="gGrid_Delete" />
+                        </Columns>
+                    </Rock:Grid>
+
+                    <asp:Panel ID="pnlManageVisit" runat="server" Visible="true">
+                        <Rock:ModalDialog ID="mdManageVisit" runat="server" SaveButtonText="Save" CancelLinkVisible="true" OnSaveClick="mdManageVisit_SaveClick">
+                            <Content>
+                                <h3>Editing <asp:Label runat="server" ID="lblModalPersonName" />'s Visit</h3>
+
+                                <Rock:CampusPicker ID="cpCampus" runat="server" Label="Campus" />
+
+                                <Rock:Toggle ID="tglBringingSpouse" runat="server" Label="Bringing Spouse?" OnText="Yes" OffText="No" />
+
+                                <Rock:Toggle ID="tglBringingChildren" runat="server" Label="Bringing Children?" OnText="Yes" OffText="No" />
+
+
+                                <asp:HiddenField ID="hfModalVisitId" runat="server" Value="" />
+
+                            </Content>
+                        </Rock:ModalDialog>
+                    </asp:Panel>
+
+                </div>
+            </div>
+        </asp:Panel>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
+
---
+++ RockWeb/Plugins/church_ccv/Cms/PlanAVisitGrid.ascx.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using church.ccv.CCVCore.PlanAVisit.Model;
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.church_ccv.Cms
+{
+    [DisplayName( "Plan A Visit Grid" )]
+    [Category( "CCV > Cms" )]
+    [Description( "Grid used to display / manage Plan A Visit" )]
+
+    public partial class PlanAVisitGrid : RockBlock
+    {
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            //InitFilter();
+            InitGrid();
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                //BindFilter();
+                BindGrid();
+            }
+
+        }
+
+        #region Events
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            BindGrid();
+        }
+
+        protected void gGrid_RowSelected( object sender, RowEventArgs e )
+        {
+            RockContext rockContext = new RockContext();
+
+            PlanAVisit visit = new Service<PlanAVisit>( rockContext ).Get( e.RowKeyId );
+
+            Person person = new PersonAliasService( rockContext ).Get( visit.PersonAliasId ).Person;
+
+            lblModalPersonName.Text = person.FullName;
+
+            cpCampus.SelectedValue = visit.CampusId.ToString();
+            tglBringingSpouse.Checked = visit.BringingSpouse;
+            tglBringingChildren.Checked = visit.BringingChildren;
+
+            hfModalVisitId.Value = e.RowKeyId.ToString();
+
+            mdManageVisit.Show();
+        }
+
+        /// <summary>
+        /// Handles the gGrid_Delete click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void gGrid_Delete( object sender, RowEventArgs e )
+        {
+            RockContext rockContext = new RockContext();
+
+            Service<PlanAVisit> pavService = new Service<PlanAVisit>( rockContext );
+
+            PlanAVisit visit = pavService.Get( e.RowKeyId );
+
+            pavService.Delete( visit );
+
+            rockContext.SaveChanges();
+
+            BindGrid();
+        }
+
+        protected void mdManageVisit_SaveClick( object sender, EventArgs e )
+        {
+            RockContext rockContext = new RockContext();
+
+            PlanAVisit visit = new Service<PlanAVisit>( rockContext ).Get( int.Parse( hfModalVisitId.Value ) );
+
+            if ( cpCampus.SelectedValue.IsNotNullOrWhitespace())
+            {
+                visit.CampusId = int.Parse( cpCampus.SelectedValue );
+            }
+
+            visit.BringingSpouse = tglBringingSpouse.Checked;
+            visit.BringingChildren = tglBringingChildren.Checked;
+
+            rockContext.SaveChanges();
+
+            mdManageVisit.Hide();
+
+            BindGrid();
+        }
+
+        #endregion
+
+        #region Grid Methods
+
+        void InitGrid()
+        {
+            gGrid.DataKeyNames = new string[] { "Id" };
+
+            gGrid.Actions.Visible = true;
+            gGrid.Actions.Enabled = true;
+            gGrid.Actions.ShowBulkUpdate = false;
+            gGrid.Actions.ShowCommunicate = false;
+            gGrid.Actions.ShowExcelExport = true;
+            gGrid.Actions.ShowMergePerson = false;
+            gGrid.Actions.ShowMergeTemplate = false;
+
+            gGrid.GridRebind += gGrid_Rebind;
+        }
+
+        private void gGrid_Rebind( object sender, EventArgs e )
+        {
+            //BindFilter();
+            BindGrid();
+        }
+
+        protected void gGrid_Edit( object sender, RowEventArgs e )
+        {
+            var qryParams = new Dictionary<string, string>
+            {
+                { "PlanAVisitInstanceId", e.RowKeyId.ToString() }
+            };
+
+            NavigateToLinkedPage( "DetailPage", qryParams );
+        }
+
+        private void BindGrid()
+        {
+            RockContext rockContext = new RockContext();
+
+            Service<PlanAVisit> pavService = new Service<PlanAVisit>( rockContext );
+
+            var pavQuery = pavService.Queryable().AsNoTracking();
+
+
+            var filteredQuery = pavQuery.AsEnumerable().Select( pav => 
+                                    new {
+                                        Id = pav.Id,
+                                        ScheduledDate = pav.ScheduledDate,
+                                        ServiceTime = GetServiceTime(pav.ServiceTimeScheduleId),
+                                        Campus = CampusCache.All().SingleOrDefault( a => a.Id == pav.CampusId ).Name,
+                                        Person = GetPersonLink(pav.PersonAliasId),
+                                        BringingSpouse = pav.BringingSpouse ? "Yes" : "No",
+                                        BringingChildren = pav.BringingChildren ? "Yes" : "No",
+                                        AttendedDate = pav.AttendedDate
+                                    } ).ToList();
+
+            gGrid.DataSource = filteredQuery;
+
+            gGrid.DataBind();
+
+        }
+
+        /// <summary>
+        /// Returns a clickable person link using a person alias Id
+        /// </summary>
+        /// <param name="personAliasId"></param>
+        /// <returns></returns>
+        private object GetPersonLink( int personAliasId )
+        {
+            Person person = new PersonAliasService( new RockContext() ).Get( personAliasId ).Person;
+
+            return String.Format( "<a href=/person/{0}>{1}</a>", person.Id, person.FullName);
+        }
+
+        /// <summary>
+        /// Return the time from a schedule using the schedule Id
+        /// </summary>
+        /// <param name="serviceTimeScheduleId"></param>
+        /// <returns></returns>
+        private object GetServiceTime( int serviceTimeScheduleId )
+        {
+            Schedule schedule = new ScheduleService( new RockContext() ).Get( serviceTimeScheduleId);
+
+            // only return the time. IE Saturday 4:00pm returns just 4:00pm
+            return schedule.Name.Substring( schedule.Name.IndexOf( ' ' ) + 1 );
+        }
+
+        #endregion
+
+
+ 
+    }
+}
