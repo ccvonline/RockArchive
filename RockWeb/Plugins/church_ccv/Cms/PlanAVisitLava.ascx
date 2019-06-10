1bdaa61d6027f2ca6e1fd104d27e8e20bc6c0e1b
Christopher Upshir <christopherupshir@ccv.church>
2019-06-10 15:50:36 -0700

    Feature cu plan a visit (#310)
    * Moved PlanAVisit to its own plugin folder PAV.
    Moved lava file to theme folder.
    Styling tweaks for landing page.
    
    * Plan A Visit production deployment, there was a conflict and it was easiest to create a new branch for deploying.  See PAV development branch for details.
    
    * Moved out string message to variable outside function call.
    
    
    Former-commit-id: 6f561926500f183d72af2e08d9d8ae214b0b870e

--- RockWeb/Plugins/church_ccv/Cms/PlanAVisitLava.ascx
+++
@@ -1,9 +0,0 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PlanAVisitLava.ascx.cs" Inherits="RockWeb.Plugins.church_ccv.Cms.PlanAVisitLava" %>
-
-<asp:UpdatePanel ID="upnlContent" runat="server">
-    <ContentTemplate>
-
-            <asp:Literal ID="lContents" runat="server" />
-
-    </ContentTemplate>
-</asp:UpdatePanel>
--- church.ccv.CCVCore/PlanAVisit/Model/PlanAVisit.cs
+++ church.ccv.CCVCore/PlanAVisit/Model/PlanAVisit.cs
@@ -19,7 +19,7 @@ namespace church.ccv.CCVCore.PlanAVisit.Model
         public int FamilyId { get; set; }
 
         [DataMember]
-        public int CampusId { get; set; }
+        public int ScheduledCampusId { get; set; }
 
         [DataMember]
         public DateTime? ScheduledDate { get; set; }
@@ -32,6 +32,9 @@ namespace church.ccv.CCVCore.PlanAVisit.Model
 
         [DataMember]
         public string SurveyResponse { get; set; }
+
+        [DataMember]
+        public int? AttendedCampusId { get; set; }
 
         [DataMember]
         public DateTime? AttendedDate { get; set; }
