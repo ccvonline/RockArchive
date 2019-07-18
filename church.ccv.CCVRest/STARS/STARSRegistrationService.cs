76d69b7819b021651b315d67eeaa3a10cd0fbea8
Christopher Upshir <christopherupshir@ccv.church>
2019-07-18 16:03:33 -0700

    Feature starsv2 registration (#379)
    * STARS API endpoint for new website register page.
    -Returns active calendar items that have registration start date before Now and End Date after Now
    -Does not include items that have new attribute value of "Private"
    -Does not include calendar items that do not have values for all new attributes used by the new website.
    
    * new lava file used by register page to display active calendar items for Stars registration.
    
    * renamed Ages property to Division
    removed unused libraries
    
    * Renamed Ages to Division
    
    * In progress backup of grid and grid filter
    
    * Created lava file for register details page
    
    * Added calendar Id query parameter to STARS ActiveRegistrations api endpoint.
    
    * Commented out filter methods (Circling back to finish at a later time).
    Updated api call to include the calendar Id.
    
    * Changed register details page to use register.css.
    Matched registration about info to designs.
    Started register-details.lava.
    
    * Added include comment
    
    * Missing logo file
    
    * position fix for debug information
    
    * Updated to utilize new image locations
    
    * Register details lava files and desktop styling.
    
    * renamed to match attributes
    
    * Asset accidentally added to wrong location, cleaning up.
    
    * Added h3 for mobile view
    
    * Moved populating register-details.lava file into _populate-register-details.lava so that it can be used on the registration form page.
    
    * Styling for h3 name in mobile view of details page
    
    * Styling for registration form page.
    
    * Added header to registration form.
    Added gender and division to header.
    Minor styling tweaks.
    
    * Work in progress mobile registration grid.
    
    * Fixed misspelled word
    
    * tweaked some font common sizes
    
    * Completed mobile grid
    
    * Adjust all font size in mobile view to keep things consistent.
    
    * Changed mobile grid to display selected items in the grid, but not be clickable, so that the end user is aware of what section they are looking at.
    
    * Styling for mobile view
    
    * Removed title content paragraph.
    
    * Added SlotsAvailable and WaitListEnabled to return data for STARS ActiveRegistrations api endpoint.
    
    * Added styling for secondary button
    
    * Added "wait list" header that is shown if registration is full and waitlist is enabled.  Also will show a different button style for wait list vs register.
    
    * Finalized Mobile Grid
    More info button indicates if sport is on wait list.
    
    * optimized image size
    
    
    Former-commit-id: 9030107a9711dc6f63f2c6d389a081c96e475a16

--- church.ccv.CCVRest/STARS/STARSRegistrationService.cs
+++ church.ccv.CCVRest/STARS/STARSRegistrationService.cs
@@ -15,7 +15,7 @@ namespace church.ccv.CCVRest.STARS
             // return object
             List<STARSRegistrationModel> activeRegistrations = new List<STARSRegistrationModel>();
 
-            // get the stars calendar and active calendar items that have occurences
+            // get the stars calendar and active calendar items that have occurrences
             EventCalendarService calendarService = new EventCalendarService( new RockContext() );
             EventCalendar starsCalendar = calendarService.Get( calendarId );
 
@@ -35,23 +35,32 @@ namespace church.ccv.CCVRest.STARS
                     foreach ( var linkage in linkages )
                     {
                         // skip linkage if we already have its occurence in the return object
-                        if ( activeRegistrations.Any( a => a.EventOccurenceId == occurence.Id ) ) {
+                        if ( activeRegistrations.Any( a => a.EventOccurrenceId == occurence.Id ) ) {
                             continue;
                         }
 
                         calendarItem.LoadAttributes();
 
+                        // skip if private
+                        string privateLink = calendarItem.AttributeValues["PrivateLink"].Value;
+
+                        if ( privateLink == "True" )
+                        {
+                            continue;
+                        }
+
+                        // skip if any of these attributes are empty
                         string sport = calendarItem.AttributeValues["Sport"].Value;
                         string gender = calendarItem.AttributeValues["Gender"].Value;
                         string division = calendarItem.AttributeValues["Division"].Value;
                         string season = calendarItem.AttributeValues["Season"].Value;
-                        string privateLink = calendarItem.AttributeValues["PrivateLink"].Value;
 
-                        // skip if private or any of these attributes are empty
-                        if ( privateLink == "True" || sport.IsNullOrWhiteSpace() || gender.IsNullOrWhiteSpace() || division.IsNullOrWhiteSpace() || season.IsNullOrWhiteSpace() )
+                        if (sport.IsNullOrWhiteSpace() || gender.IsNullOrWhiteSpace() || division.IsNullOrWhiteSpace() || season.IsNullOrWhiteSpace() )
                         {
                             continue;
                         }
+
+                        int slotsAvailable = linkage.RegistrationInstance.MaxAttendees - linkage.RegistrationInstance.Registrations.Count;
 
                         if ( occurence.Campus == null)
                         {
@@ -68,13 +77,15 @@ namespace church.ccv.CCVRest.STARS
                                     // campus found that offers sport of calendar item
                                     STARSRegistrationModel registration = new STARSRegistrationModel()
                                     {
-                                        EventOccurenceId = occurence.Id,
+                                        EventOccurrenceId = occurence.Id,
                                         RegistrationInstanceId = linkage.RegistrationInstanceId,
                                         Campus = campus.Name,
                                         Sport = sport,
                                         Gender = gender,
                                         Division = division,
-                                        Season = season
+                                        Season = season,
+                                        SlotsAvailable = slotsAvailable > 0 ? slotsAvailable : 0,
+                                        WaitListEnabled = linkage.RegistrationInstance.RegistrationTemplate.WaitListEnabled
                                     };
 
                                     activeRegistrations.Add( registration );
@@ -86,13 +97,15 @@ namespace church.ccv.CCVRest.STARS
                             // single campus specified in occurence
                             STARSRegistrationModel registration = new STARSRegistrationModel()
                             {
-                                EventOccurenceId = occurence.Id,
+                                EventOccurrenceId = occurence.Id,
                                 RegistrationInstanceId = linkage.RegistrationInstanceId,
                                 Campus = occurence.Campus.Name,
                                 Sport = sport,
                                 Gender = gender,
                                 Division = division,
-                                Season = season
+                                Season = season,
+                                SlotsAvailable = slotsAvailable > 0 ? slotsAvailable : 0,
+                                WaitListEnabled = linkage.RegistrationInstance.RegistrationTemplate.WaitListEnabled
                             };
 
                             activeRegistrations.Add( registration );
