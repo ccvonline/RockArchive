14bf02b0bd509cfc134dfbba77270d95cd3dfb6a
BEN TWEEDY <ben@btpaz.com>
2019-09-30 15:11:01 -0700

    remove code comments
    Former-commit-id: 96c13213b1bc01f4016d1008f0d107f21727a294

--- RockWeb/Plugins/church_ccv/PAV/ScheduleAVisitForm.ascx.cs
+++ RockWeb/Plugins/church_ccv/PAV/ScheduleAVisitForm.ascx.cs
@@ -22,8 +22,6 @@ namespace RockWeb.Plugins.church_ccv.PAV
     [Description( "Form used to preregister families for a weekend service" )]
     [DefinedValueField( Rock.SystemGuid.DefinedType.PERSON_CONNECTION_STATUS, "Connection Status", "The connection status to use for new individuals (default: 'Web Prospect'.)", true, false, Rock.SystemGuid.DefinedValue.PERSON_CONNECTION_STATUS_WEB_PROSPECT, "", 0 )]
     [DefinedValueField( Rock.SystemGuid.DefinedType.PERSON_RECORD_STATUS, "Record Status", "The record status to use for new individuals (default: 'Pending'.)", true, false, Rock.SystemGuid.DefinedValue.PERSON_RECORD_STATUS_PENDING, "", 1 )]
-    //[GroupLocationTypeField( Rock.SystemGuid.GroupType.GROUPTYPE_FAMILY, "Address Type",
-        //"The type of address to be displayed / edited.", false, Rock.SystemGuid.DefinedValue.GROUP_LOCATION_TYPE_HOME, "", order: 2 )]
     [CampusesField( "Campuses", "Campuses that offer visit scheduling", false, "", "", 3 )]
     [SchedulesField( "Service Schedules", "Service Schedules available for use", true, "", "", 4 )]
     [SystemEmailField( "Confirmation Email Template", "System email template to use for the email confirmation.", true, "", "", 5 )]
