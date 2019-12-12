02bf0191f51e439191541570961ec80a47b4d547
Mason Kinyon <mavhuntrx@gmail.com>
2019-12-12 14:15:34 -0700

    Updated cleanup job to be more performant.
    Former-commit-id: d293921f4e7484f15075a30cd7a8066431a10e7c

--- church.ccv.SafetySecurity/Jobs/VolunteerScreeningCleanup.cs
+++ church.ccv.SafetySecurity/Jobs/VolunteerScreeningCleanup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,31 +54,40 @@ namespace church.ccv.SafetySecurity.Jobs
         public int DeleteExpiredVolunteerScreenings( int retentionPeriod )
         {
             int count = 0;
-            var rockContext = new RockContext();
-            var vsService = new VolunteerScreeningService( rockContext );
-
+            List<int> screeningIds = new List<int>();
+            
             // get all volunteer screenings that have a workflow that is complete.
-            var screenings = vsService.Queryable()
-                .Where( vs => vs.Application_Workflow != null &&
-                              !vs.Application_Workflow.CompletedDateTime.HasValue )
-                .ToList();
+            using ( var rockContext = new RockContext() )
+            {
+                screeningIds = new VolunteerScreeningService( rockContext ).Queryable()
+                        .AsNoTracking()
+                        .Where( vs => vs.Application_Workflow != null &&
+                                      !vs.Application_Workflow.CompletedDateTime.HasValue )
+                        .Select( vs => vs.Id )
+                        .ToList(); 
+            }
 
             // loop through and remove any that are past the retention period and are still "Waiting for Applicant"...
-            foreach ( var screening in screenings )
+            foreach ( var screeningId in screeningIds )
             {
-                if ( screening.CreatedDateTime < RockDateTime.Now.AddDays( -1 * ( int ) retentionPeriod ) )
+                using ( var rockContext = new RockContext() )
                 {
-                    var workflow = screening.Application_Workflow;
-                    if ( workflow != null )
+                    var vsService = new VolunteerScreeningService( rockContext );
+                    var screening = vsService.Get( screeningId );
+                    if ( screening.CreatedDateTime < RockDateTime.Now.AddDays( -1 * ( int ) retentionPeriod ) )
                     {
-                        string state = VolunteerScreening.GetState( workflow.CreatedDateTime, workflow.ModifiedDateTime, workflow.Status );
-
-                        if ( state == "Waiting for Applicant to Complete" )
+                        var workflow = screening.Application_Workflow;
+                        if ( workflow != null )
                         {
-                            vsService.Delete( screening );
-                            rockContext.SaveChanges();
+                            string state = VolunteerScreening.GetState( workflow.CreatedDateTime, workflow.ModifiedDateTime, workflow.Status );
 
-                            count++;
+                            if ( state == "Waiting for Applicant to Complete" )
+                            {
+                                vsService.Delete( screening );
+                                rockContext.SaveChanges();
+
+                                count++;
+                            }
                         }
                     }
                 }
@@ -95,25 +105,34 @@ namespace church.ccv.SafetySecurity.Jobs
         public int DeleteExpiredCharacterReferences( int retentionPeriod )
         {
             int count = 0;
-            var rockContext = new RockContext();
-            var workflowService = new WorkflowService( rockContext );
+            List<int> workflowIds = new List<int>();
 
             // get all character reference workflows that are incomplete.
-            var workflowsToDelete = workflowService.Queryable()
-                .Where( w => w.CompletedDateTime == null &&
-                             w.Status != "Completed" &&
-                             w.WorkflowTypeId == 203 )
-                .ToList();
+            using ( var rockContext = new RockContext() )
+            {
+                workflowIds = new WorkflowService( rockContext ).Queryable()
+                        .AsNoTracking()
+                        .Where( w => w.CompletedDateTime == null &&
+                                     w.Status != "Completed" &&
+                                     w.WorkflowTypeId == 203 )
+                        .Select( w => w.Id )
+                        .ToList(); 
+            }
 
             // loop through and remove any that are past the retention period.
-            foreach ( var workflow in workflowsToDelete )
+            foreach ( var workflowId in workflowIds )
             {
-                if ( workflow.CreatedDateTime < RockDateTime.Now.AddDays( -1 * ( int ) retentionPeriod ) )
+                using ( var rockContext = new RockContext() )
                 {
-                    workflowService.Delete( workflow );
-                    rockContext.SaveChanges();
+                    var workflowService = new WorkflowService( rockContext );
+                    var workflow = workflowService.Get( workflowId );
+                    if ( workflow.CreatedDateTime < RockDateTime.Now.AddDays( -1 * ( int ) retentionPeriod ) )
+                    {
+                        workflowService.Delete( workflow );
+                        rockContext.SaveChanges();
 
-                    count++;
+                        count++;
+                    } 
                 }
             }
 
